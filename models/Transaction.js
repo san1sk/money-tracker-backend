24c0bf16e0d8e6df49f78fafd350c0dad92ae1f6
Sanchit Khokhar <88581704+san1sk@users.noreply.github.com>
2024-03-14 14:01:02 +0530

    initiall commit

---
+++ models/Transaction.js
@@ -0,0 +1,16 @@
+// import {model,Schema} from "mongoose";
+const mongoose = require('mongoose');
+const {Schema,model}=mongoose;
+
+const TransactionSchema=new Schema({
+    name:{type:String,required:true},
+    price:{type:Number,required:true},
+    description:{type:String,required:true},
+    datetime:{type:Date,required:true},
+});
+
+const TransactionModel=model('Transaction',TransactionSchema);
+
+module.exports=TransactionModel;
+
+
